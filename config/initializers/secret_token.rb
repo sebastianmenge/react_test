279ea225ea41380f52deea32311def07dded9709
sebastianmenge <s.menge@gmx.net>
2014-03-23 15:43:26 +0100

    add some stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReactTest::Application.config.secret_key_base = 'd16edbad2c1a363443f593d7bc748abd87263a09cc6a920e624fe32c4e19be12921aa931cb0da821234b06b01408123e33203cf7928c5092474cdbd5e3098681'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ReactTest::Application.config.session_store :cookie_store, key: '_react_test_session'
